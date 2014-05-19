87da16e11c9bb299ae86bc54495e1b7b94a5a968
Sairam Sankaran <sairam.sankaran@gmail.com>
2014-05-19 09:07:12 -0700

    BST draft added

---
+++ src/main/java/com/sankaran/sairam/BST.java
@@ -0,0 +1,25 @@
+package com.sankaran.sairam;
+
+public class BST {
+    private TreeNode root = null;
+
+    public void insertNode(TreeNode node) {
+
+    }
+
+    public void deleteNode(TreeNode node) {
+
+    }
+
+    public void preOderTraversal() {
+
+    }
+
+    public void inOderTraversal() {
+
+    }
+
+    public void postOderTraversal() {
+
+    }
+}
